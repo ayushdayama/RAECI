eabfda327a4f3eb0d69e11e4354162ff74376a00
Ayush Dayama <ayushdayama097@gmail.com>
2023-03-02 12:47:49 +0530

    Added a session manager

--- js/homepageJS.js
+++ js/homepageJS.js
@@ -1,6 +1,49 @@
-var selectedValue = sessionStorage.getItem("selectedValue");
-function changeHeader() {
-    document.getElementById("welcomeHeader").innerHTML = "हेल्लो " + selectedValue + " जी";
+// Add the check-in data to the Firestore database
+const firebaseConfig = {
+    apiKey: "AIzaSyAVW7vzCOMunDqJylPHgtaJytnYhUJRMzQ",
+    authDomain: "raeci0.firebaseapp.com",
+    databaseURL: "https://raeci0-default-rtdb.firebaseio.com",
+    projectId: "raeci0",
+    storageBucket: "raeci0.appspot.com",
+    messagingSenderId: "789419507030",
+    appId: "1:789419507030:web:0bcf5b7971d1c2e8ed6af7",
+    measurementId: "G-J1JQDR2BVR"
+};
+
+// Init Firebase and Ref to DB
+firebase.initializeApp(firebaseConfig);
+
+// Retrieve the sessionId value from the URL parameter
+const urlParams = new URLSearchParams(window.location.search);
+const sessionId = urlParams.get('sessionId');
+
+// Listen for changes to the Firebase database
+firebase.database().ref('sessions/' + sessionId).on('value', function (snapshot) {
+    // Check if selectedValue is present in sessionStorage
+    var selectedValue = sessionStorage.getItem("selectedValue");
+
+    // If selectedValue is not present in sessionStorage, retrieve it from the Firebase database
+    if (selectedValue === null) {
+        selectedValue = snapshot.child("selectedValue").val();
+    }
+
+    // Update sessionStorage with the retrieved value
+    sessionStorage.setItem("selectedValue", selectedValue);
+
+    // Update the welcome header with the selected value
+    changeHeader(selectedValue);
+});
+
+function checkSessionStorage() {
+    // Retrieve the selectedValue from the sessionStorage
+    var selectedValue = sessionStorage.getItem("selectedValue");
+    if (selectedValue !== null) {
+        changeHeader(selectedValue);
+    }
+}
+
+function changeHeader(selectedValue) {
+    document.getElementById("welcomeHeader").innerHTML = "हेल्लो<br>" + selectedValue + " जी";
 }
 
 function loadPastData() {
@@ -19,8 +62,15 @@ function loadPastData() {
     });
 }
 
+function logout() {
+    localStorage.removeItem('loggedIn');
+    localStorage.removeItem('user');
+    firebase.database().ref('sessions/' + sessionId).remove();
+    sessionStorage.removeItem("selectedValue");
+    window.location.href = 'index.html';
+}
+
 function handleCheckin() {
-    // Get the current date and time
     const date = new Date();
     const dateString = `${date.getDate().toString().padStart(2, '0')}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getFullYear()}`;
     const timeString = date.toLocaleTimeString('en-GB');
